faf4a6c09dbb94aa9cc1bfd7ba2e98ba1d3469bc
Jameson Proctor <jameson@athleticsnyc.com>
2015-06-13 22:31:42 -0400

    Adding composer.json and src.

---
+++ Athletics/Manifest/LogLady.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Athletics\Manifest;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+class LogLady extends Logger {
+
+	public function __construct( $args = array() ) {
+
+		$defaults = array(
+			'name'	 => 'Manifest',
+			'path'	 => ABSPATH . 'wp-content/manifest.log',
+			'level'	 => Logger::DEBUG
+		);
+
+		$args = wp_parse_args( $args, $defaults );
+
+		parent::__construct( 
+			$args['name'], 
+			array( new StreamHandler( $args['path'], $args['level'] ) 
+		) );
+
+	}
+
+}

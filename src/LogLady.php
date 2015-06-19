f211ad30160cc5d35f2c6e9453605d5db2f1fde0
jp1971 <jameson@athleticsnyc.com>
2015-06-19 00:04:31 -0400

    Addin comments. Modifying defaults.

--- src/LogLady.php
+++ src/LogLady.php
@@ -5,13 +5,37 @@ namespace Athletics\Manifest;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
+
+/**
+ * LogLady
+ *
+ * A Monolog plugin for WordPress
+ *
+ * @author Athletics <wordpress@athleticsnyc.com>
+ */
 class LogLady extends Logger {
 
+	/**
+     * @param array $args {
+     *
+ 	 *     Optional. An array of arguments.
+ 	 *
+ 	 *     @type string  name   The logging channel.       Default value is 
+ 	 *                          'LogLady'.
+ 	 *
+ 	 *     @type string  path   The path to the log file.  Default value is
+ 	 *                          ABSPATH . 'wp-content/log-lady.log'
+ 	 *
+ 	 *     @type string  level  The logging level.         Default value is
+ 	 *							Logger::DEBUG
+ 	 *     
+ 	 * }
+     */
 	public function __construct( $args = array() ) {
 
 		$defaults = array(
-			'name'	 => 'Manifest',
-			'path'	 => ABSPATH . 'wp-content/manifest.log',
+			'name'	 => 'LogLady',
+			'path'	 => ABSPATH . 'wp-content/log-lady.log',
 			'level'	 => Logger::DEBUG
 		);
 

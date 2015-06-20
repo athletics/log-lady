9c0e2556311a82bd081698133910c29740377b26
jp1971 <jameson@athleticsnyc.com>
2015-06-19 23:59:31 -0400

    Writing initial test.

--- tests/LogLadyTest.php
+++ tests/LogLadyTest.php
@@ -1,1 +1,36 @@
-LogLadyTest.php
+<?php
+
+namespace Athletics\Manifest\LogLady;
+
+class LogLadyTest extends \PHPUnit_Framework_TestCase {
+
+    protected $logLady = null;
+
+    /**
+     * Perform test setup
+     */
+    public function setUp() {
+
+        $this->logLady = new LogLady();
+    
+    }
+ 
+    /**
+     * Test default name
+     */
+    public function testDefaultName() {
+
+        $this->assertEquals( 'LogLady', $this->logLady->getName() );
+
+    }
+
+    /**
+     * Perform test teardown
+     */
+    public function tearDown() {
+        
+        $this->logLady = null;
+    
+    }
+
+}
